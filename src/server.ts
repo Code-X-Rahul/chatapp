11c6073e8c5ad8975f151d2487ad5c3c1fd4d94e
Rahul Rajput <rr611305@gmail.com>
2023-08-28 23:07:44 +0530

    feat :socket io setup with basic chat app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,47 @@
 import express from "express";
-const app = express();
+import http from "http";
+import { Server } from "socket.io";
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.get("/", (req: any, res: any) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
+app.get("/hello", (req: any, res: any) => {
+  res.sendFile(__dirname + "/view/personalChat.html");
 });
 
-app.listen(3000);
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+
+  const count = io.of("/").sockets.size;
+  console.log(count);
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg: string) => {
+    io.emit("chat message", msg);
+  });
+});
+
+const personalChat = io.of("/personal-chat");
+
+personalChat.on("connection", (socket: any) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("personal message", (msg: string) => {
+    personalChat.emit("personal message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
