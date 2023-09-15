f9d9cb10c879a3b63ebf31318c8bf2e7ce5ff4b0
Rahul Rajput <rr611305@gmail.com>
2023-09-15 17:34:22 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// Define the path to your frontend folder
-const frontendPath = path.join(__dirname, "..", "frontend");
-
-// Serve static files from the "frontend" folder
-app.use(express.static(frontendPath));
 
 // Define your API routes and other backend logic here
 
