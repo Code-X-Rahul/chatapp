3b3eaee1aca93e9d411fe6c24a1de2a466794c1f
Rahul Rajput <rr611305@gmail.com>
2023-09-18 12:29:20 +0530

    making backend live

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const connectDB = require("./db/connect");
 
 //  routers
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
