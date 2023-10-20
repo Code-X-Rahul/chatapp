1d1b55a481f073cf289a5eb4a4760e49cf56b170
Rahul Rajput <rr611305@gmail.com>
2023-10-20 19:53:59 +0530

    cors fix

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://chatapp-backend-alpha.vercel.app",
+  // baseURL: "http://localhost:5000",
   withCredentials: true,
+  headers: {
+    "Cache-Control": "no-cache",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
