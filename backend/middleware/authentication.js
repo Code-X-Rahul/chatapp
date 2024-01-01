3184ce7ccaef52725d183ade0aa4456b52ec66de
Rahul Rajput <rr611305@gmail.com>
2024-01-01 13:26:12 +0530

    console.log removed

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -3,10 +3,7 @@ const { isTokenValid } = require("../utils");
 const Token = require("../models/Token");
 const { attachCookiesToResponse } = require("../utils");
 const authenticateUser = async (req, res, next) => {
-  // console.log(req);
   const { refreshToken, accessToken } = req.signedCookies;
-  console.log(req);
-  console.log(refreshToken, accessToken);
   try {
     if (accessToken) {
       const payload = isTokenValid(accessToken);
